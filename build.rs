a8a6cecedce975655db8643ff52d7c6fbb850859
0xC0DE666 <0xC0DE666@proton.me>
2025-10-28 13:29:47 +0200

    added xai-protos as gi submodule

--- build.rs
+++ build.rs
@@ -5,18 +5,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .out_dir(&"src/")
         .compile_protos(
             &[
-                "proto/xai/api/v1/auth.proto",
-                "proto/xai/api/v1/chat.proto",
-                "proto/xai/api/v1/deferred.proto",
-                "proto/xai/api/v1/documents.proto",
-                "proto/xai/api/v1/embed.proto",
-                "proto/xai/api/v1/image.proto",
-                "proto/xai/api/v1/models.proto",
-                "proto/xai/api/v1/sample.proto",
-                "proto/xai/api/v1/tokenize.proto",
-                "proto/xai/api/v1/usage.proto",
+                "xai/proto/xai/api/v1/auth.proto",
+                "xai/proto/xai/api/v1/chat.proto",
+                "xai/proto/xai/api/v1/deferred.proto",
+                "xai/proto/xai/api/v1/documents.proto",
+                "xai/proto/xai/api/v1/embed.proto",
+                "xai/proto/xai/api/v1/image.proto",
+                "xai/proto/xai/api/v1/models.proto",
+                "xai/proto/xai/api/v1/sample.proto",
+                "xai/proto/xai/api/v1/tokenize.proto",
+                "xai/proto/xai/api/v1/usage.proto",
             ],
-            &["proto"], // Include dir for imports
+            &["xai/proto"], // Include dir for imports
         )?;
     Ok(())
 }
